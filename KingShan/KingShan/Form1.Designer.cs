745344f33ec005f7e27faaa09afaa7cac03d4eef
oddizzo <oddizzo@gmail.com>
2017-04-05 16:03:18 +0200

    Commit KingShan Project
    Commit KingShan Project

---
+++ KingShan/KingShan/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace KingShan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblOut = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Choose Weather Condition:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(55, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Sunny\r\n";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(52, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Rainy";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 65);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(55, 17);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Windy";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(16, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Enter Orbit1 Traffic Speed e.g10:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(19, 153);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Enter Orbit2 Traffic Speed e.g20:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(19, 197);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(122, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(19, 241);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 13);
+            this.lblOutput.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Output";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblOut
+            // 
+            this.lblOut.AutoSize = true;
+            this.lblOut.Location = new System.Drawing.Point(16, 250);
+            this.lblOut.Name = "lblOut";
+            this.lblOut.Size = new System.Drawing.Size(0, 13);
+            this.lblOut.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 376);
+            this.Controls.Add(this.lblOut);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblOut;
+
+    }
+}
+
---
+++ KingShan/KingShan/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KingShan
+{
+    public partial class Form1 : Form
+    {
+        double bikeTraffSped = 10;
+        double tuktukTraffSped = 12;
+        double carTraffSped = 20;
+        double obit1TraffSped;
+        double obit2TraffSped;
+        string weather;
+        string obitOuput;
+        string vehicleOuput;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            weather = "sunny";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            weather = "rainy";
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            weather = "windy";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            obit1TraffSped = double.Parse(textBox1.Text);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            obit2TraffSped = double.Parse(textBox2.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (weather != null)
+            {
+                if (obit1TraffSped != 0)
+                {
+                    if (obit2TraffSped != 0)
+                    {
+                        if (obit1TraffSped > obit2TraffSped)
+                        {
+                            obitOuput = "Orbit1";
+                            if (weather == "sunny")
+                            {
+                                if (obit1TraffSped >= carTraffSped)
+                                {
+                                    vehicleOuput = "Car";
+                                }
+                                else if (obit1TraffSped >= tuktukTraffSped)
+                                {
+                                    vehicleOuput = "Tuk Tuk";
+                                }
+                                else
+                                {
+                                    vehicleOuput = "Bike";
+                                }
+                            }
+                            else if (weather == "rainy")
+                            {
+                                if (obit1TraffSped >= carTraffSped)
+                                {
+                                    vehicleOuput = "Car";
+                                }
+                                else
+                                {
+                                    vehicleOuput = "Tuk Tuk";
+                                }
+                            }
+                            else if (weather == "windy")
+                            {
+                                if (obit1TraffSped >= carTraffSped)
+                                {
+                                    vehicleOuput = "Car";
+                                }
+                                else if (obit1TraffSped >= tuktukTraffSped)
+                                {
+                                    vehicleOuput = "Tuk Tuk";
+                                }
+                                else
+                                {
+                                    vehicleOuput = "Bike";
+                                }
+                            }
+                            lblOut.Text = "Vehicle " + vehicleOuput + " on " + obitOuput;
+                        }
+                        else
+                        {
+                            obitOuput = "Orbit2";
+                            if (weather == "sunny")
+                            {
+                                if (obit2TraffSped >= carTraffSped)
+                                {
+                                    vehicleOuput = "Car";
+                                }
+                                else if (obit2TraffSped >= tuktukTraffSped)
+                                {
+                                    vehicleOuput = "Tuk Tuk";
+                                }
+                                else
+                                {
+                                    vehicleOuput = "Bike";
+                                }
+                            }
+                            else if (weather == "rainy")
+                            {
+                                if (obit2TraffSped >= carTraffSped)
+                                {
+                                    vehicleOuput = "Car";
+                                }
+                                else
+                                {
+                                    vehicleOuput = "Tuk Tuk";
+                                }
+                            }
+                            else if (weather == "windy")
+                            {
+                                if (obit2TraffSped >= carTraffSped)
+                                {
+                                    vehicleOuput = "Car";
+                                }
+                                else if (obit2TraffSped >= tuktukTraffSped)
+                                {
+                                    vehicleOuput = "Tuk Tuk";
+                                }
+                                else
+                                {
+                                    vehicleOuput = "Bike";
+                                }
+                            }
+                            lblOut.Text = "Vehicle " + vehicleOuput + " on " + obitOuput;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Enter Orbit2 Traffic Speed");
+                        textBox2.Focus();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Enter Orbit1 Traffic Speed");
+                    textBox1.Focus();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Choose weather condition.");
+            }
+        }
+    }
+}
